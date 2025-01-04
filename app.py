773043f895f66f7064e496b9da29eed25f9ced4b
Devil-Aura <imh203356@gmail.com>
2025-01-04 15:11:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from config import cfg
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "TechVJ - Bot is Running!"
+    return 'TechVJ Bot is Running!'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=cfg.FLASK_PORT)
