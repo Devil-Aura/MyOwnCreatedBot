3776212283f0f757e0dfd9e251f4dd160816f2f4
Devil-Aura <imh203356@gmail.com>
2025-01-04 15:31:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+from os import getenv  # Add this line to fix the error
+
 class Config:
     API_ID = int(getenv("API_ID", "22768311"))
     API_HASH = getenv("API_HASH", "702d8884f48b42e865425391432b3794")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    CHID = int(getenv("CHID", "-1002432405855"))  # Channel ID
+    CHID = int(getenv("CHID", "-1002432405855"))
     SUDO = list(map(int, getenv("SUDO", "6040503076").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://<tyuvie>:<lxYx2uM6elNxd9BR>@cluster0.ybi1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    FLASK_PORT = int(getenv("FLASK_PORT", 8080))  # Default Flask port
+    FLASK_PORT = int(getenv("FLASK_PORT", 8080))  # Changed port to 8080
 cfg = Config()
