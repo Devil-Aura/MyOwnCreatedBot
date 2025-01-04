4b12e14bc4c44da715edc30172499f0bde1daf87
Devil-Aura <imh203356@gmail.com>
2025-01-04 15:10:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-from os import getenv
-
 class Config:
     API_ID = int(getenv("API_ID", "22768311"))
     API_HASH = getenv("API_HASH", "702d8884f48b42e865425391432b3794")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    CHID = int(getenv("CHID", "-1002432405855"))  # Ensure bot is admin here
+    CHID = int(getenv("CHID", "-1002432405855"))  # Channel ID
     SUDO = list(map(int, getenv("SUDO", "6040503076").split()))
-    MONGO_URI = getenv("MONGO_URI", ""mongodb+srv://<tyuvie>:<lxYx2uM6elNxd9BR>@cluster0.ybi1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://<tyuvie>:<lxYx2uM6elNxd9BR>@cluster0.ybi1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    FLASK_PORT = int(getenv("FLASK_PORT", 5000))  # Default Flask port
 cfg = Config()
