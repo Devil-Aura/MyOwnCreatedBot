db7db12dc90009a3a405a704a1ba666d754472fe
Devil-Aura <imh203356@gmail.com>
2025-01-14 21:35:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TechVJ Bot is Running!'
+    return 'World_Fastest Bot is Running!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=cfg.FLASK_PORT)
