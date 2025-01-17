91995a9d6e483631ecf08894ee37b4cceaf8dd12
Devil-Aura <imh203356@gmail.com>
2025-01-17 18:29:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ class Config:
     API_ID = int(getenv("API_ID", "22768311"))
     API_HASH = getenv("API_HASH", "702d8884f48b42e865425391432b3794")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    CHID = int(getenv("CHID", "-1002432405855"))
+    FORCE_SUB_CHANNEL = getenv("FORCE_SUB_CHANNEL", "World_Fastest_Bots")  # Force subscription channel
     SUDO = list(map(int, getenv("SUDO", "6040503076").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://iamrealdevil098:M7UXF0EL3M352q0H@cluster0.257nd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1002446826368"))  # Log channel to track user and group activities
     FLASK_PORT = int(getenv("FLASK_PORT", 9543))  # Changed port to 8080
+
 cfg = Config()
