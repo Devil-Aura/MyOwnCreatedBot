22d9e0e9949db89c2b8135dd61df8d42ec4d4f4d
Devil-Aura <imh203356@gmail.com>
2025-04-10 20:59:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ class Config:
     # Your Force Subscribe Channel ID (Make Bot Admin In This Channel)
     CHID = int(getenv("CHID", "-1002432405855"))  
 
+    LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1002446826368"))
+    
     # Admin Or Owner IDs (Multiple Admins Can Be Added)
     SUDO = list(map(int, getenv("SUDO", "6040503076").split()))
 
