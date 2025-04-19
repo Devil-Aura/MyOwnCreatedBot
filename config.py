856233180f6264ad867472431bde0b7d2acf8090
Devil-Aura <imh203356@gmail.com>
2025-04-20 00:20:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,17 +21,6 @@ class Config:
     # Default Welcome Message
     DEFAULT_WELCOME_MSG = "🎉 Welcome, {user_mention}!\nYour request to join {chat_title} has been approved! 🚀In 0.5 Seconds, This Is World Fastest Request Approval Bot.\n/start (For Use Me)"
     
-    WELCOME_TEXT = (
-    "<b>🤗 Hello {user_mention}!</b>\n\n"
-    "🚀 I am the <b>FASTEST BOT</b>, faster than light ⚡!<br>"
-    "I approve join requests in just <b>0.5 seconds</b>.<br><br>"
-    "<blockquote>"
-    "I'm an auto-approve <a href='https://t.me/telegram/153'>Admin Join Requests</a> Bot.<br>"
-    "I can approve users in Groups/Channels.<br>"
-    "Add me to your chat and promote me to admin with 'Add Members' permission."
-    "</blockquote><br><br>"
-    "<b>Powered By :</b> <a href='https://t.me/World_Fastest_Bots'>@World_Fastest_Bots</a>"
-)
 
 
 cfg = Config()
